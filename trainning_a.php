60a344f0ea3bd56025e97431b79c2342c87049a2
Gill <gilcpd2@gmail.com>
2015-03-25 20:59:24 -0300

    120

--- trainning_a.php
+++ trainning_a.php
@@ -30,7 +30,7 @@ require('inc/sidebar.php');
                     <?php
                     error_reporting(E_ALL);
 
-                    $mysqli = new mysqli("localhost", "root", "12OfAd9c6bNDfcBV", "zadmin_fitwiz");
+                    $mysqli = new mysqli("127.0.0.1", "root", "12OfAd9c6bNDfcBV", "zadmin_fitwiz");
                     if ($mysqli->connect_errno) {
                         echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
                     }
